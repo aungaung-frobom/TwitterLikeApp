aa0c62a210d26666afbdb236de141003316c5ac3
aungaung-frobom <aung.aung@frobom.com>
2017-12-29 12:06:05 +0630

    upgrate smtp server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,17 +76,20 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   # fro mailling using smtp server
+  
 config.action_mailer.raise_delivery_errors = true 
 config.action_mailer.delivery_method = :smtp 
 config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
- config.action_mailer.smtp_settings = {   
-    address: 'smtp.gmail.com', 
-       port: 587, 
-      domain: 'gmail.com',
-      user_name: frobosakai@gmail.com,
-      password: frobosakai123,
-      authentication: 'plain', 
-      enable_starttls_auto: true }
+ 
+config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            'frobosakai@gmail.com',
+  password:             'frobosakai123',
+  authentication:       :plain,
+  enable_starttls_auto: true
+}
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
