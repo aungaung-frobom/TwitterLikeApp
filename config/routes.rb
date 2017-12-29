fded82f064aad979813d47bc00ddd8a463d942c8
aungaung-frobom <aung.aung@frobom.com>
2017-12-29 11:54:12 +0630

    init

---
+++ config/routes.rb
@@ -0,0 +1,45 @@
+Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
+  get 'password_reset/new'
+
+  get 'password_reset/edit'
+
+  get 'sessions/new'
+
+  get 'users/new'
+
+ root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help', as: 'helf'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
+  get  '/login',   to: 'sessions#new'
+  post  '/login',   to: 'sessions#create'
+  delete  '/logout',  to: 'sessions#destroy'
+   resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
+end
+=begin
+	
+rescue Exception => e
+	
+end
+ get 'static_pages/home'
+
+ get 'static_pages/help'
+get 'static_pages/about'
+get 'static_pages/contact'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ #root 'application#hello'
+  end
+=end
